728fa3371827499cb049319ff10bce2f435b2b52
forzamilan <pterus.2@gmail.com>
2017-05-27 23:34:56 +0000

    ready for testing

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:query', function (req, res) {
+  var query = req.params.query;
+  var unix = null;
+  var natural = null;
+  
+  if (+query >= 0) {
+    unix = +query;
+    natural = unixToNat(unix);
+  } 
+  
+  if (isNaN(+query) && moment(query, "MMMM D, YYYY").isValid()) {
+    unix = +natToUnix(query);
+    natural = unixToNat(unix);
+  }
+  var readyDate = { "unix": unix, "natural": natural };
+  res.send(readyDate);
+  
 })
 
+function natToUnix(date) {
+  // Conver from natural date to unix timestamp
+  return moment(date, "MMMM D, YYYY").format("X");
+}
+    
+function unixToNat(unix) {
+  // Convert unix timestamp to natural date
+  return moment.unix(unix).format("MMMM D, YYYY");
+}
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
